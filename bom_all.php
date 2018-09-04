a16b7a0bd3a93962e86ac4aa498665ace6723c32
Nattawut212 <31862875+Nattawut212@users.noreply.github.com>
2018-09-05 00:44:25 +0700

    Add files via upload

---
+++ bom_all.php
@@ -0,0 +1,68 @@
+<!--<?php include ("bb.php");?>-->
+<head>
+<title>Bom</title></head>
+<body bgcolor=>
+<table width=100% bgcolor=pink><tr><td>
+<span style="font-size:30px;">Hello</span>
+</td></tr></table>
+<table width=100% bgcolor=#dddddd><tr>
+<td><a href='../myweb.php'>home</a></td>
+<td><a href='../multi_form.php'>multiple</a></td>
+<td><a href='../friend.php'>friend</a></td>
+<td><a href='../me.php'>about us</a></td>
+<td><a href="../logout.php">logout</a></td>
+</tr></table>
+<body>
+
+<style>
+@font-face{
+font-family:mono;
+src:url('mono.ttf');
+}
+</style>
+<pre style="font-family:mono;font-size:20px;">
+
+ <center>
+ <table border="1" width="1040"   align="center">
+
+<tr><td> <a href=bom_01.php>bom1</a><br>
+<a href=bom_02.php>bom2</a><br>
+<a href=bom_03.php>bom3</a><br>
+<a href=bom_04.php>bom4</a><br>
+<a href=bom_05.php>bom5</a><br>
+<a href=bom_06.php>bom6</a><br>
+<a href=bom_07.php>bom7</a><br>
+<a href=bom_08.php>bom8</a><br>
+<a href=bom_09.php>bom9</a><br>
+<a href=bom_10.php>bom10</a><br>
+<a href=bom_11.php>bom11</a><br>
+<a href=bom_12.php>bom12</a><br>
+<a href=bom_13.php>bom13</a><br>
+<a href=bom_14.php>bom14</a><br>
+<a href=bom_15.php>bom15</a><br>
+<a href=bom_16.php>bom16</a><br>
+<a href=bom_17.php>bom17</a><br>
+<a href=bom_18.php>bom18</a><br>
+<a href=bom_19.php>bom19</a><br>
+<a href=bom_20.php>bom20</a><br>
+<a href=bom_21.php>bom21</a><br>
+<a href=bom_22.php>bom22</a><br>
+<a href=bom_24.php>bom24</a><br>
+<a href=bom_25.php>bom25</a><br>
+<a href=bom_26.php>bom26</a><br>
+<a href=bom_27.php>bom27</a><br>
+<a href=bom_28.php>bom28</a><br>
+<a href=bom_29.php>bom29</a><br>
+<a href=bom_30.php>bom30</a><br>
+<a href=bom_31.php>bom31</a><br>
+<a href=bom_32.php>bom32</a><br>
+<a href=bom_33.php>bom33</a><br>
+<a href=bom_35.php>bom35</a><br>
+
+<?php
+include ('myweb_footer.php');
+?>
+</td></tr>
+</table>
+ </center>
+</pre></body>
---
+++ me.php
@@ -0,0 +1,4 @@
+Nattawut pinjai
+lampang <br>
+<a href="myweb.php">home</a>
+<a href="friend.php">friend us</a>
